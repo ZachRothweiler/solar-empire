3e954ad7fecbfd026804d15473143eff0d1dd0ec
Zach Rothweiler <zachrothweiler@gmail.com>
2023-04-10 21:59:03 -0400

    - moved web code to src/php and sql code to src/sql

--- docs/refreshdb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/usr/local/mysql/bin/mysql -u solarempire --password=solarempire solarempire < sql/new_server.sql
-/usr/local/mysql/bin/mysql -u solarempire --password=solarempire solarempire < sql/new_game.sql
